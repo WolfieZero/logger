113128d72c2030c615b85d00af8e3c941e9198a6
Neil Sweeney <neil@jennie-en1.manor.fubra.net.manor.fubra.net>
2011-09-16 14:54:50 +0100

    first commit

---
+++ logger.php
@@ -0,0 +1,24 @@
+<?php
+
+define('LOG_FILE',	'log');
+define('MODE',		'a');
+define('DATE',		'H:m:s - m.d.y - ');
+
+class logger {
+
+	private $fp;
+	
+	function __construct() {
+		$this->fp = fopen(LOG_FILE, MODE);
+	}
+
+	function log($log) {
+		$write = date(DATE).$log."\r\n";
+		fwrite($this->fp, $write);
+	}
+
+	function close() {
+		fclose($this->fp);
+	}
+
+}
